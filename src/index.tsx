be389319e43ceeaf9762aca9e967d85dbfe02829
wojciek <wojtekgluch@gmail.com>
2020-10-18 12:27:03 +0200

    fix for exports

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
+Amplify.configure({ ...awsExports, ssr: true });
 
 ReactDOM.render(
   <React.StrictMode>
